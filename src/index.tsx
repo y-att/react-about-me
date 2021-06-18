723c162c6d67b4ef715ab015eeafb3ecf1ef1148
Yunas A <yatteyeh@gmail.com>
2021-06-18 10:38:20 +0300

    1st

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,4 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("react-app-root"));
+ReactDOM.render(<PageFooter />, document.getElementById("page-footer"));
